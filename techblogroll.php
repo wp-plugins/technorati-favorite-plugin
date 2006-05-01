cf8202bc3e06565e1dcb8fd132fcf44cb76af073
switchhook <switchhook@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-05-01 03:02:18 +0000

    Added code to make this a Widget
    git-svn-id: https://plugins.svn.wordpress.org/technorati-favorite-plugin/trunk@5975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- techblogroll.php
+++ techblogroll.php
@@ -18,50 +18,106 @@ Or you can do this if you want an un ordered list
 
 <?php  technoratiFavoriteList("<username>", "ul"); ?>
 
-This plugin will use the Wordpress 2.0 Caching system 
+This plugin will use the Wordpress 2.0 Caching system
 */
 
-function technoratiFavoriteList($technorati_user, $type="ol", $cache_time = 600) {
-
-    if (!$technorati_user) {
+function faves_list_init() {
+    // Check for the required API functions
+    if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') ) {
         return;
     }
-    if (strtolower($type) == "ol") {
-        $t =  "ol";
-    } elseif (strtolower($type) == "ul") {
-        $t = "ul";
-    } else {
-        $t = "ol";
-    }
-    $api_url = "http://feeds.technorati.com/faves/" . $technorati_user . "?type=list&format=xoxo&t=$t";
 
-    $cacheKey = "techblogroll.$technorati_user.$type";
-
-    wp_cache_init();
-    $data = wp_cache_get($cacheKey);
-    if (!$data) {
-
-        $c = curl_init($api_url);
-        curl_setopt($c, CURLOPT_RETURNTRANSFER,1);
-        curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 2);
-        curl_setopt($c, CURLOPT_TIMEOUT, 4);
-        curl_setopt($c, CURLOPT_USERAGENT, "Technorati blogroll plugin");
-        $response = curl_exec($c);
-        $info = curl_getinfo($c);
-
-        $curl_error_code = $info['http_code'];
-        curl_close($c);
-        if ($curl_error_code == 200) {
-            $data = $response;
-            wp_cache_set($cacheKey,$data,'',$cache_time);
-            wp_cache_close();
-        } elseif ($curl_error_code) {
-            //do something here
-        } else {
-            //do something here
+    function faves_list_contol () {
+        $options = $newoptions = get_option('widget_faves_list');
+        if ( $_POST['faves-list-submit'] ) {
+            $newoptions['user']   = strip_tags(stripslashes($_POST['faves-list-user']));
+            $newoptions['type']   = strip_tags(stripslashes($_POST['faves-list-type']));
+            $newoptions['title']  = strip_tags(stripslashes($_POST['faves-list-title']));
         }
+        if ( $options != $newoptions ) {
+            $options = $newoptions;
+            update_option('widget_faves_list', $options);
+        }
+       ?>
+        <div style="text-align:right">
+             <label for="faves-list-title" style="line-height:35px;display:block;">Title: <input type="text" id="faves-list-title" name="faves-list-title" value="<?php echo htmlspecialchars($options['title']); ?>" /></label>
+             <label for="faves-list-user" style="line-height:35px;display:block;">User: <input type="text" id="faves-list-user" name="faves-list-user" value="<?php echo htmlspecialchars($options['user']); ?>" /></label>
+             <label for="faves-list-type" style="line-height:35px;display:block;">Type (optional): <input type="text" id="faves-list-type" name="faves-list-type" value="<?php echo htmlspecialchars($options['type']); ?>" /></label>
+             <input type="hidden" name="faves-list-submit" id="faves-list-submit" value="Save" />
+             <input type="submit" value="Save" />
+        </div>
+   <?php
     }
-    echo $data;
+    function technoratiFavoriteList() {
+
+
+        $options = (array) get_option('widget_faves_list');
+
+        $technorati_user   = $options['user'];
+        $type              = $options['type'];
+        $title             = $options['title'];
+        $cache_time        = $options['cache_time'];
+
+
+        if (!$technorati_user) {
+            return;
+        }
+
+        if (!$type) {
+            $type = "ul";
+        }
+
+        if (! isset($cache_time)) {
+            $cache_time = 600;
+        }
+
+        if (strtolower($type) == "ol") {
+            $t =  "ol";
+        } elseif (strtolower($type) == "ul") {
+            $t = "ul";
+        } else {
+            $t = "ol";
+        }
+        $api_url = "http://feeds.technorati.com/faves/" . $technorati_user . "?type=list&format=xoxo&t=$t";
+
+        $cacheKey = "techblogroll.$technorati_user.$type";
+
+        wp_cache_init();
+        $data = wp_cache_get($cacheKey);
+        if (!$data) {
+
+            $c = curl_init($api_url);
+            curl_setopt($c, CURLOPT_RETURNTRANSFER,1);
+            curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 2);
+            curl_setopt($c, CURLOPT_TIMEOUT, 4);
+            curl_setopt($c, CURLOPT_USERAGENT, "Technorati blogroll plugin");
+            $response = curl_exec($c);
+            $info = curl_getinfo($c);
+
+            $curl_error_code = $info['http_code'];
+            curl_close($c);
+            if ($curl_error_code == 200) {
+                $data = $response;
+                wp_cache_set($cacheKey,$data,'',$cache_time);
+                wp_cache_close();
+            } elseif ($curl_error_code) {
+                //do something here
+            } else {
+                //do something here
+            }
+        }
+        echo "<h2>$title</h2>\n";
+        echo "<ul>";
+
+        echo $data;
+        echo "</ul>";
+
+    }
+    register_sidebar_widget('Technorati Faves', 'technoratiFavoriteList');
+    register_widget_control('Technorati Faves', 'faves_list_contol');
 }
 
+
+
+add_action('plugins_loaded', 'faves_list_init');
 ?>
